7a6e273063186190b03a9a542bd27d7b87ddc86a
David Torres <datcx@hotmail.com>
2021-04-13 08:18:56 -0500

    Update gpx.js

--- backend/src/models/gpx.js
+++ backend/src/models/gpx.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose;
 const  GPXSchema = new Schema({
     latitude: { type: String, required: true},
     longitude: { type: String, required: true},
-    elevacion: {type:String, required: false},
-    nombre: { type: String, required: true},
+    elevation: {type:String, required: false},
+    name: { type: String, required: true},
     distance: { type: String, required: true},
     danger: { type: String}, //??? Si lo ponemos? Que significa?
 });
